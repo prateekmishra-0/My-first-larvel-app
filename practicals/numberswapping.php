4479ffb3561ea1f27709e812b5c10f0e0d883118
prateekmishra-0 <mprateek580@gmail.com>
2023-12-02 17:20:41 +0530

    Update Number swapping program for readability and maintainability

--- practicals/numberswapping.php
+++ practicals/numberswapping.php
@@ -1,21 +1,30 @@
+@@ -1,16 +1,21 @@
 <?php
-// Function to swap two numbers
+
+$number1 = 10;
+$number2 = 20;
+// Define a function to swap two numbers by reference
 function swapNumbers(&$a, &$b) {
-    $temp = $a;
-    $a = $b;
-    $b = $temp;
+    $temp = $a;  // Store the value of $a in a temporary variable
+    $a = $b;     // Update the value of $a with the value of $b
+    $b = $temp;  // Update the value of $b with the value from the temporary variable
 }
 
-// Example usage
+echo "Original Numbers: ";
+echo "Number 1: $number1, Number 2: $number2";
 $number1 = 5;
 $number2 = 10;
 
-echo "Before swapping: ";
-echo "Number 1: $number1, Number 2: $number2";
+$temp = $number1;
+$number1 = $number2;
+$number2 = $temp;
+echo "Before swapping: Number 1 = $number1, Number 2 = $number2<br>";
 
-// Call the swapNumbers function
+// Call the swapNumbers function to swap the values of $number1 and $number2
 swapNumbers($number1, $number2);
 
-echo "<br>After swapping: ";
+echo "After swapping: Number 1 = $number1, Number 2 = $number2";
+
+echo "<br>Swapped Numbers: ";
 echo "Number 1: $number1, Number 2: $number2";
 ?>
